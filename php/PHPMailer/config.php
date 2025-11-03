e5c08dae370962e423bd033efdfa2381c1de0425
emiliaalzararee <emilia.al-zararee@outlook.com>
2025-11-03 19:25:53 +0200

    php poistettu ja lisätty inbound sales sertifikaatti

--- php/PHPMailer/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php - säilyy palvelimella, ei GitHubissa
-return [
-    'host' => 'smtp.gmail.com',               // tai 'smtp.office365.com' Outlookille
-    'username' => 'ealzararee@gmail.com', 
-    'password' => 'orcruidzlpqvwdpe', // Gmailin sovelluskohtainen salasana
-    'port' => 587,
-    'secure' => 'tls'
-];
